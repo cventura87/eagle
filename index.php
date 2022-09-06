f4c53ef7c1b0b2a2f2b9529de979dedadbe42355
Ciro Ventura <65572755+cventura87@users.noreply.github.com>
2022-09-06 17:26:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
+ <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
 <?php 
 
 $a = (isset($_REQUEST['a'])) ? $_REQUEST['a'] : null;
 switch ($a) {
 	case 'login':
-	include_once('login.php');
+	filexist('login.html');
+	//include_once('login.php');
 	break;
 
 	case 'count':
-	include_once('count.php');
+	filexist('count.html');
+	//include_once('count.php');
 	break;
 
 
 	case 'about':
-	include_once('about.html');
+	filexist('about.html');
+	//include_once('about.html');
 	break;
 
 	case 'contact':
-	include_once('contacts.html');
+	filexist('contacts.html');
+	//include_once('contacts.html');
 	break;
 
 	default:
-	require_once('home.html');
+filexist('home.html');
+	//require_once('home.html');
 	break;
 }
 
+function filexist($file){
+	if (file_exists($file)) {
+		return true;
+		require_once($file);
+	}else{
+		echo'<div class="alert alert-danger alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <strong>Message</strong> error al abrir la secuencia: no existe el archivo o directorio <strong>'.$file.'</strong>
+              </div>';
+	}
+}
 ?>
+
+
 
